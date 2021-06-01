7e306286cdf67e97442012211c848274d6b7da5f
guilherme-funchal <guilherme.funchal@gmail.com>
2021-06-01 11:25:36 -0300

    Login e Logout #1
    Inclusão de login e logout

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,70 @@
+dmx.config({
+  "components": {
+    "sessionStorage": [
+      {
+        "type": "text",
+        "name": "email"
+      }
+    ],
+    "localStorage": [
+      {
+        "type": "text",
+        "name": "email"
+      }
+    ],
+    "cookies": [
+      {
+        "type": "text",
+        "name": "email"
+      }
+    ]
+  },
+  "componentes": {
+    "cookies": [
+      {
+        "type": "text",
+        "name": "email"
+      }
+    ]
+  },
+  "storage": {
+    "cookies": [
+      {
+        "type": "text",
+        "name": "email"
+      }
+    ],
+    "sessionStorage": [
+      {
+        "type": "text",
+        "name": "email"
+      }
+    ],
+    "localStorage": [
+      {
+        "type": "text",
+        "name": "email"
+      }
+    ]
+  },
+  "teste": {
+    "localStorage": [
+      {
+        "type": "text",
+        "name": "token"
+      }
+    ],
+    "sessionStorage": [
+      {
+        "type": "text",
+        "name": "token"
+      }
+    ],
+    "cookies": [
+      {
+        "type": "text",
+        "name": "token"
+      }
+    ]
+  }
+});
