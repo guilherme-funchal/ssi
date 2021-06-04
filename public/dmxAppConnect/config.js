bca82e183cf9b3af50917736e0e8a30b36268acb
guilherme-funchal <guilherme.funchal@gmail.com>
2021-06-04 16:38:44 -0300

    Login e Logout #1
    Inclusão de token na sessão, login e logoff

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -66,5 +66,56 @@ dmx.config({
         "name": "token"
       }
     ]
+  },
+  "users": {
+    "data_detail1": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "text"
+        },
+        {
+          "name": "first_name",
+          "type": "text"
+        },
+        {
+          "name": "last_name",
+          "type": "text"
+        },
+        {
+          "name": "email",
+          "type": "text"
+        },
+        {
+          "name": "document",
+          "type": "text"
+        },
+        {
+          "name": "role",
+          "type": "text"
+        },
+        {
+          "name": "did",
+          "type": "text"
+        },
+        {
+          "name": "verkey",
+          "type": "text"
+        },
+        {
+          "name": "pass_hash",
+          "type": "text"
+        },
+        {
+          "name": "pass_salt",
+          "type": "text"
+        },
+        {
+          "name": "token",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    }
   }
 });
