183be28156471d2c61eef1ccd76c286743aa31d3
Guilherme Funchal da Silva <guilherme.funchal@gmail.com>
2021-07-02 09:17:47 -0300

    CRUD Credentials #3
    CRUD Credentials #3

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -175,6 +175,226 @@ dmx.config({
         }
       ],
       "outputType": "array"
+    },
+    "data_detail2": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "$value"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_detail_schema_id": {
+      "meta": [
+        {
+          "type": "object",
+          "name": "schema",
+          "sub": [
+            {
+              "type": "text",
+              "name": "ver"
+            },
+            {
+              "type": "text",
+              "name": "id"
+            },
+            {
+              "type": "text",
+              "name": "name"
+            },
+            {
+              "type": "text",
+              "name": "version"
+            },
+            {
+              "type": "array",
+              "name": "attrNames",
+              "sub": [
+                {
+                  "type": "text",
+                  "name": "$value"
+                }
+              ]
+            },
+            {
+              "type": "number",
+              "name": "seqNo"
+            }
+          ]
+        }
+      ],
+      "outputType": "object"
+    },
+    "sessionStorage": [
+      {
+        "type": "array",
+        "name": "data"
+      },
+      {
+        "type": "object",
+        "name": "headers"
+      },
+      {
+        "type": "text",
+        "name": "id"
+      },
+      {
+        "type": "text",
+        "name": "connection_id"
+      },
+      {
+        "type": "text",
+        "name": "creddef_id"
+      }
+    ],
+    "repeat1": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "$value"
+        }
+      ],
+      "outputType": "array"
+    },
+    "data_detail_creddef": {
+      "meta": [
+        {
+          "type": "array",
+          "name": "credential_definition_ids",
+          "sub": [
+            {
+              "type": "text",
+              "name": "$value"
+            }
+          ]
+        }
+      ],
+      "outputType": "object"
+    },
+    "credencial-form": [
+      {
+        "type": "object",
+        "name": "data",
+        "sub": [
+          {
+            "type": "boolean",
+            "name": "auto_issue"
+          },
+          {
+            "type": "boolean",
+            "name": "auto_remove"
+          },
+          {
+            "type": "text",
+            "name": "comment"
+          },
+          {
+            "type": "text",
+            "name": "connection_id"
+          },
+          {
+            "type": "text",
+            "name": "cred_def_id"
+          },
+          {
+            "type": "object",
+            "name": "credential_preview",
+            "sub": [
+              {
+                "type": "text",
+                "name": "@type"
+              },
+              {
+                "type": "array",
+                "name": "attributes",
+                "sub": [
+                  {
+                    "type": "text",
+                    "name": "name"
+                  },
+                  {
+                    "type": "text",
+                    "name": "value"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "type": "boolean",
+            "name": "trace"
+          }
+        ]
+      }
+    ]
+  },
+  "credentials": {
+    "repeat1": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "cpf"
+        }
+      ],
+      "outputType": "object"
+    },
+    "var1": {
+      "meta": null,
+      "outputType": "text"
+    },
+    "arr1": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "rg"
+        }
+      ],
+      "outputType": "object"
+    },
+    "data_detail1": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "referent"
+        },
+        {
+          "type": "object",
+          "name": "attrs",
+          "sub": [
+            {
+              "type": "text",
+              "name": "cpf"
+            }
+          ]
+        },
+        {
+          "type": "text",
+          "name": "schema_id"
+        },
+        {
+          "type": "text",
+          "name": "cred_def_id"
+        },
+        {
+          "type": "text",
+          "name": "rev_reg_id"
+        },
+        {
+          "type": "text",
+          "name": "cred_rev_id"
+        }
+      ],
+      "outputType": "array"
+    },
+    "repeat2": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "rg"
+        }
+      ],
+      "outputType": "object"
     }
   }
 });
